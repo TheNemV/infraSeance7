00f426747a00347c338c192c2245d81ccd0a8a4a
TheNemV <37929078+TheNemV@users.noreply.github.com>
2020-10-26 12:00:20 +0100

    Delete accueil.php

--- RessourcesSeance7/siteBonneNouvelles/views/accueil.php
+++
@@ -1,5 +0,0 @@
-		<section id="contenu">
-			<h2>Accueil oh yeah</h2>
-			<p>Bienvenue aux séances d'exercices consacrées à élaborer un site Internet en PHP selon une architecture didactique MVC OO.</p>
-			<p>Jean-Luc Collinet.</p>
-		</section>
